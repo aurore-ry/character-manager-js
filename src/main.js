2da15b1b7a12a86010ee58d8d354b8870dee9135
aurore-ry <aremy.dev@gmail.com>
2021-10-25 16:59:47 +0200

    in progress

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 const url = "https://character-database.becode.xyz/characters";
 let encode = encodeURI(url);
+
+const cardTemplate = document.querySelector(".hero-card");
+
 console.log("encoded= ", encode);
 
-const get = async (url) => {
-  return await (await fetch(url)).json();
+const get = async (url, id) => {
+  try {
+    return await (await fetch(url + id)).json();
+  } catch (error) {
+    console.error(error.message);
+  }
 };
+
+console.log("get=", get);
 
 const post = async (url, data) => {
   const response = await fetch(url, {
@@ -25,6 +34,16 @@ const del = async (url) => {
 
 // main page character
 
+// display all
+const home = async () => {
+  const characters = await get(url, "");
+  characters.forEach((character) => {
+    const currentCharacter = document.importNode(cardTemplate.content, true);
+    const img = document.querySelector("img.avatar-hero-card");
+  });
+};
+
+// -- Main page open form for add character --
 const openForm = () => {
   document.querySelector(".form-container").style.display = "flex";
 };
