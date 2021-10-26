59d964e5c8ac7dc1d9c2382a7cc65b09ada0be42
Meulemans Philippe <meulemansph@hotmail.com>
2021-10-26 12:20:21 +0200

    character editor working

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const url = "https://character-database.becode.xyz/";
+const url = "https://character-database.becode.xyz/characters";
 
 const get = async (url) => {
   return await (await fetch(url)).json();
@@ -186,6 +186,16 @@ const del = async (url) => {
 
 
 
+const put = async (url, data, id) => {
+  const response = await fetch(url+"/"+testid, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response;
+};
 
 
 
@@ -194,22 +204,71 @@ const del = async (url) => {
 
 
 
-
-
+let testid="dcaf3afb-3f20-4e7b-90a9-55b3eaafaf1d";
 
 //Start Phil
+let submitIndex = document.querySelector("#submit_index");
+let submitButton = document.querySelector("#submit_button");
 
-let submitButton = document.querySelector("#submit-button");
-console.log(submitButton);
-submitButton.addEventListener("click", ()=>{
+let nameField = document.querySelector("#a");
+let shortDescField = document.querySelector("#b");
+let longDescField = document.querySelector("#c");
 
-  let editNameInput= document.querySelector("#input__Name");
-  let editShortDescInput= document.querySelector("#input__shortDesc");
-  let editlongDescInput= document.querySelector("#input__longDesc");
+nameField.value="";
+shortDescField.value="";
+longDescField.value="";
 
-  console.log(editNameInput);
+submitIndex.addEventListener("click", async()=>{
+  /* let heroeIndex = document.querySelector("#submit_index").value; */
 
-}
+
+  let testall= await get(url);
+  let test= await get(url+"/"+testid);
+
+  
+  /* console.log(test); */
+  /* console.log(testall); */
+  let editImage = test.image;
+  let editName = test.name;
+  let editShortDesc = test.shortDescription;
+  let editLongDesc =  test.description;
+  document.querySelector("#i").src=("data:image/jpeg;base64,"+editImage);
+  nameField.value= editName;
+  shortDescField.value= editShortDesc;
+  longDescField.value= editLongDesc;
+
+  console.log(editName);
+
+  
+})
+
+submitButton.addEventListener("click", async()=>{
+  /* let imageInput = document.querySelector("#i").src; */
+  let test= await get(url+"/"+testid);
+  
+  
+  let imageInput = test.image;
+
+  let nameInput = document.querySelector("#a").value;
+  let editShortDescInput = document.querySelector("#b").value;
+  let editLongDescInput = document.querySelector("#c").value;
+  
+  
+  console.log(nameInput);
+  console.log(editShortDescInput);
+  console.log(editLongDescInput);
+  
+  let editHeroe = new Object;
+  editHeroe.id = testid;
+  editHeroe.image = imageInput;
+  editHeroe.name = nameInput;
+  editHeroe.shortDescription = editShortDescInput;
+  editHeroe.description = editShortDescInput;
+
+  console.log(editHeroe);
+  let update = await put(url,editHeroe,testid);
+  
+  
 
 
-);
+});
