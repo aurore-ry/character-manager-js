c07073c4c2baaf22775bf9939cd58bbfb3fc9fc3
aurore-ry <aremy.dev@gmail.com>
2021-10-26 11:47:05 +0200

    get data from server and display it

--- src/main.js
+++ src/main.js
@@ -1,19 +1,26 @@
-const url = "https://character-database.becode.xyz/characters";
-let encode = encodeURI(url);
+const url = "https://character-database.becode.xyz";
 
-const cardTemplate = document.querySelector(".hero-card");
+const cardTemplate = document.querySelector("#card-template");
 
-console.log("encoded= ", encode);
-
-const get = async (url, id) => {
+const get = async (url) => {
   try {
-    return await (await fetch(url + id)).json();
+    const req = await fetch(url);
+    const data = await req.json();
+    return data;
   } catch (error) {
     console.error(error.message);
   }
 };
 
-console.log("get=", get);
+const getAllCharacters = async () => {
+  const characters = await get(`${url}/characters`);
+  return characters;
+};
+
+const getCharacterById = async (id) => {
+  const character = await get(`${url}/characters/${id}`);
+  return character;
+};
 
 const post = async (url, data) => {
   const response = await fetch(url, {
@@ -32,14 +39,36 @@ const del = async (url) => {
   });
 };
 
-// main page character
-
 // display all
 const home = async () => {
-  const characters = await get(url, "");
+  const char1 = await getCharacterById("a4d08a81-d02b-4545-97f8-87ee7bdb190b");
+  console.log("char1=", char1.name);
+
+  const characters = await getAllCharacters();
+  const parentEl = document.querySelector(".hero-wrapper");
+
   characters.forEach((character) => {
-    const currentCharacter = document.importNode(cardTemplate.content, true);
-    const img = document.querySelector("img.avatar-hero-card");
+    const templateNode = cardTemplate.content.firstElementChild.cloneNode(true);
+
+    const currentCharacter = document
+      .createElement("div")
+      .appendChild(templateNode);
+    console.log(currentCharacter);
+    currentCharacter.setAttribute("id", character.id);
+
+    const img = currentCharacter.querySelector("img.avatar-hero-card");
+    img.setAttribute("src", `data:image/png;base64,${character.image}`);
+    img.setAttribute("width", "100px");
+    img.setAttribute("height", "100px");
+
+    const name = currentCharacter.querySelector(".hero-name-card");
+    name.textContent = character.name;
+    parentEl.appendChild(currentCharacter);
+
+    const smallDescription = currentCharacter.querySelector(
+      ".hero-description-card"
+    );
+    smallDescription.textContent = character.shortDescription;
   });
 };
 
@@ -50,3 +79,5 @@ const openForm = () => {
 const closeForm = () => {
   document.querySelector(".form-container").style.display = "none";
 };
+
+home();
